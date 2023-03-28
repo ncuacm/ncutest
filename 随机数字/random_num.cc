20efc3c2c17ca57672c89c0c1211bf010a928955
linuxChan <2213663986@qq.com>
2023-03-28 15:53:11 +0800

    demo

---
+++ 随机数字/random_num.cc
@@ -0,0 +1,15 @@
+/**
+//  @file random_num.cc
+//  @author SuoNi Chan (suoni1919@gmail.com)
+//  @date 2023-03-28
+//  生成随机数字
+//  @copyright Copyright (SuoNi Chan) 2023
+//  
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main () {
+    
+}
