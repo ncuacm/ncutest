c1b87f8ba76ce1fd57dd708a2cd594f2607e6b57
linuxChan <2213663986@qq.com>
2023-03-28 20:59:52 +0800

    add demo

---
+++ Demo/获得一个双浮点随机数/get_one_double_num.cc
@@ -0,0 +1,15 @@
+/**
+//  @file get_one_double_num.cc
+//  @author SuoNi Chan (suoni1919@gmail.com)
+//  @date 2023-03-28
+//  
+//  @copyright Copyright (SuoNi Chan) 2023
+//  
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    
+}
