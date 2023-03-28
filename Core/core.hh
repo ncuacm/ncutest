4b7aac90e5a2f6370dea043e12de69d64fd1ede1
linuxChan <2213663986@qq.com>
2023-03-28 20:30:12 +0800

    add demo

---
+++ Core/core.hh
@@ -0,0 +1,13 @@
+/**
+//  @file core.hh
+//  @author SuoNi Chan (suoni1919@gmail.com)
+//  @date 2023-03-28
+//  常用核心API
+//  @copyright Copyright (SuoNi Chan) 2023
+//  
+**/
+#pragma once
+
+#include <bits/stdc++.h>
+using namespace std;
+
--- Demo/get_one_random_num.cc
+++ Demo/get_one_random_num.cc
@@ -14,7 +14,7 @@ using namespace std;
 
 /**
 //  @in 无
-    @out 返回一个随机数字 (范围：int_min ~ int_max)
+    @out 返回一个随机数字 (范围：0 ~ int_max)
 
     @example
     in:
@@ -34,7 +34,18 @@ using namespace std;
 **/
 
 int main () {
-    cout << random();
+
+    cout << "请选择生成文件的份数:" << endl;
+    int T;
+    cin >> T;
+    //初始化时间种子
+    srand((unsigned)time(0));
+
+    for(int i = 1; i <= T; i++) {
+        string f_name = to_string(i)+".in";
+        ofstream OUT(f_name);
+        OUT << rand();
+    }
 
     return 0;
 }
