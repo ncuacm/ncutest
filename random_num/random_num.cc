ac818f4521d7c924feb560377b17313e908f89d6
linuxChan <2213663986@qq.com>
2023-03-28 17:30:56 +0800

    add a demo

--- random_num/random_num.cc
+++ random_num/random_num.cc
@@ -6,34 +6,3 @@
 //  @copyright Copyright (SuoNi Chan) 2023
 //  
 **/
-
-#include<bits/stdc++.h>
-using namespace std;
-
-
-/**
-//  @in 无
-    @out 返回一个随机数字 (范围：int_min ~ int_max)
-
-    @example
-    in:
-    out:
-    5
-
-    @example
-    in:
-    out:
-    99
-
-    @example
-    in:
-    out:
-    1450
-//  
-**/
-
-int main () {
-    cout << random();
-
-    return 0;
-}
