c3ba94e0beaf5a58b7805ffc1d60bc6d624186fa
linuxChan <2213663986@qq.com>
2023-03-28 22:45:06 +0800

    demo

---
+++ 数字序列/不重复的整数序列/non_repeating_integer_sequence.cc
@@ -0,0 +1,24 @@
+/**
+//  @file non_repeating_integer_sequence.cc
+//  @author SuoNi Chan (suoni1919@gmail.com)
+//  @date 2023-03-28
+//  
+//  @copyright Copyright (SuoNi Chan) 2023
+//  
+**/
+
+#include <bits/stdc++.h> 
+using namespace std;
+using ll = long long;
+
+void solve() {
+    
+}
+//
+
+int main() {
+	ll T;cin>>T;while(T--)
+    solve();
+    
+    return 0;
+}
