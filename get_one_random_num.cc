6bd8c1ed013a4fcf34ec8bb68109b48a40c58f5b
linuxChan <2213663986@qq.com>
2023-03-28 16:23:03 +0800

    Add some standards.

--- get_one_random_num.cc
+++ get_one_random_num.cc
@@ -1,15 +1,8 @@
 /**
-//  @file random_num.cc
+//  @file get_one_random_num.cc
 //  @author SuoNi Chan (suoni1919@gmail.com)
 //  @date 2023-03-28
-//  生成随机数字
+//  获得一个随机数
 //  @copyright Copyright (SuoNi Chan) 2023
 //  
 **/
-
-#include<bits/stdc++.h>
-using namespace std;
-
-int main () {
-    
-}
---
+++ random_num/random_num.cc
@@ -0,0 +1,39 @@
+/**
+//  @file random_num.cc
+//  @author SuoNi Chan (suoni1919@gmail.com)
+//  @date 2023-03-28
+//  生成随机数字
+//  @copyright Copyright (SuoNi Chan) 2023
+//  
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+/**
+//  @in 无
+    @out 返回一个随机数字 (范围：int_min ~ int_max)
+
+    @example
+    in:
+    out:
+    5
+
+    @example
+    in:
+    out:
+    99
+
+    @example
+    in:
+    out:
+    1450
+//  
+**/
+
+int main () {
+    cout << random();
+
+    return 0;
+}
